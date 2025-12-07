db28fb53a42079ba22756b61e39974b1ff3341bd
groomigroom <googjohn0143@gmail.com>
2025-12-07 09:25:16 +0900

    Update java_println.java

--- java_println.java
+++ java_println.java
@@ -4,3 +4,12 @@ class SecondConsole {
         System.out.println("구름구름세상이에요");
     }
 }
+
+class ThirdConsole {
+    public static void main(String[] args) {
+        int age = 9;
+        double number = 3.14;
+        String name = "김구름";
+        System.out.println("age: " +age+" number: "+number+" name: "+name);
+    }
+}
