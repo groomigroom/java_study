79cdf5916509e5ca590cdc1e0d6fe952d44db8cd
groomigroom <googjohn0143@gmail.com>
2026-03-18 20:31:45 +0900

    Create Math_pow.java

---
+++ Math_pow.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        double num;
+        num = Math.pow(3, 2);
+        System.out.println(num);
+        num = Math.pow(4, 3);
+        System.out.println(num);
+
+        /*
+        Java에서 Math.pow(a, b)는 밑(base)인 
+        를 지수(exponent)인 
+        만큼 거듭제곱한 값(
+        )을 계산하는 정적 메소드입니다. double 형으로 두 개의 인자를 받아 double 값을 반환하며, 2의 3제곱(
+        )은 Math.pow(2, 3)으로 작성하여 
+        의 결과를 얻습니다. 
+        double 형은 기본 실수형: Java에서 소수점 있는 숫자는 기본적으로 double로 인식됩니다.
+        */
+    }
+}
