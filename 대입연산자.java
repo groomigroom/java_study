4ccb2042ed17367ff692f9a5c1e9536883de817d
groomigroom <googjohn0143@gmail.com>
2026-03-20 17:10:57 +0900

    Create 대입연산자.java

---
+++ 대입연산자.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        int num = 20;
+        num++ ;
+        System.out.print(num+" ");
+        num-- ;
+        System.out.print(num+" ");
+        num += 3;
+        System.out.print(num+" ");
+        num -= 3;
+        System.out.print(num+" ");
+        num *= 3;
+        System.out.print(num+" ");
+        num /= 3;
+        System.out.print(num+" ");
+        num %= 3;
+        System.out.print(num+" ");
+    }
+}
