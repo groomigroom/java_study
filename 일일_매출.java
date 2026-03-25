d99292f9e51b93393427195a65edb28a5f994caf
groomigroom <googjohn0143@gmail.com>
2026-03-25 16:42:24 +0900

    Create 일일_매출.java

---
+++ 일일_매출.java
@@ -0,0 +1,17 @@
+package new_projectdd;
+
+public class good_partttt {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+
+		total -= 900*10;
+		total += 1000*2;
+		total -= 1500*22;
+		total += 2000*4;
+		
+		System.out.println("오늘 총매출액은" + total + "입니다.");
+	}
+
+}
