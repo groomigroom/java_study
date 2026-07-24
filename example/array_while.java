7997af7c3dd75df575740676832151b7062b1d52
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:20:43 +0900

    Create array_while.java

---
+++ example/array_while.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int[] array = {2, 3, 4, 5, 6};
+      int i = 0;
+
+      while (i < array.length) {
+        System.out.println("i = " + array[i]);
+        i++;
+      }
+    }
+}
