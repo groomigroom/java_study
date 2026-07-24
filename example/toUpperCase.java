d617f445f0a9ab9726874d51195e5e4a54527091
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:52:10 +0900

    Create toUpperCase.java

---
+++ example/toUpperCase.java
@@ -0,0 +1,11 @@
+public class Main {
+  private static String getString() {
+    return "groomi";
+  }
+
+  public static void main(String[] args) {
+    String isNull = getString();
+
+    System.out.println(isNull.toUpperCase());
+  }
+}
