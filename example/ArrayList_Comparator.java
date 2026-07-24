c9eda73c15a298144d65282817df047a3ed19e13
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:42:10 +0900

    Create ArrayList_Comparator.java

---
+++ example/ArrayList_Comparator.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+      ArrayList<String> list = new ArrayList<>();
+
+      list.add("groomi");
+      list.add("groomii");
+      list.add("groomiiiii");
+    }
+}
