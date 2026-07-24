50f078e5dc31346a99c26a7ab35e94ecedf2280d
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:13:47 +0900

    Update if_else_if_else.java

--- example/if_else_if_else.java
+++ example/if_else_if_else.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
       int num = 1;
 
-      if (num == 1) {}
+      if (num == 1) {
+        System.out.println("if 블록이다.");
+      }
+      else if (num == 2) {
+        System.out.println("if 블록이다.");
+      }
     }
 }
