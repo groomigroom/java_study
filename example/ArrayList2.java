9d08e6e93290a7cc925a34e4e8517b5e32444527
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:33:16 +0900

    Update ArrayList2.java

--- example/ArrayList2.java
+++ example/ArrayList2.java
@@ -4,5 +4,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
       ArrayList<String> list = new ArrayList<>();
+
+      list.add("groomi");
+      list.add("groom");
+      list.add("groomgroom");
+
+      for (int i = 0; i < list.size(); i++) {
+        System.out.println(list.get(i));
+      }
     }
 }
