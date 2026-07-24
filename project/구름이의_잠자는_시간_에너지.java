fe346a9cd689617dd0fc95f299f49d5868a404b0
groomigroom <googjohn0143@gmail.com>
2026-07-24 20:19:32 +0900

    Update 구름이의_잠자는_시간_에너지.java

--- project/구름이의_잠자는_시간_에너지.java
+++ project/구름이의_잠자는_시간_에너지.java
@@ -6,7 +6,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+      //scanner로 강아지 이름 받기??
       String puppy_name = "김구름";
+      //scanner로 강아지 수면 시간 받기??
       int a = sleepEnergy(3);
       System.out.println(puppy_name + a);
     }
