b25d644eb240b62bf1726cc876070b257f6e1578
groomigroom <googjohn0143@gmail.com>
2026-07-24 18:29:11 +0900

    Update ArrayList.java

--- example/ArrayList.java
+++ example/ArrayList.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-      List list = new ArrayList<Integer>();
+      ArrayList<Integer> list = new ArrayList<>();
 
       list.add(1);
       list.add(2);
