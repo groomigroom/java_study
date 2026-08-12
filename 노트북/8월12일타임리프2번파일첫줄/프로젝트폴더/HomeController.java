174d06d2b939d60acf5274c1fd6b267ac4ade9ef
groomigroom <googjohn0143@gmail.com>
2026-08-12 09:50:30 +0900

    Update HomeController.java

--- 노트북/8월12일타임리프2번파일첫줄/프로젝트폴더/HomeController.java
+++ 노트북/8월12일타임리프2번파일첫줄/프로젝트폴더/HomeController.java
@@ -1,1 +1,21 @@
-HomeController.java
+package groomi.groomgroom; // 본인의 패키지 경로에 맞게 수정
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Calendar;
+
+@Controller
+public class HomeController {
+
+    // ControllerMappings 클래스 대신 @GetMapping으로 URL을 매핑합니다.
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        
+        // WebContext.setVariable 대신 스프링의 Model에 데이터를 담습니다.
+        model.addAttribute("welcomeMessage", "안녕하세요! GTVG 쇼핑몰에 오신 것을 환영합니다.");
+        model.addAttribute("today", Calendar.getInstance());
+
+        // 리턴한 문자열을 바탕으로 뷰 리졸버가 templates/home.html을 찾습니다.
+        return "home";
+    }
+}
