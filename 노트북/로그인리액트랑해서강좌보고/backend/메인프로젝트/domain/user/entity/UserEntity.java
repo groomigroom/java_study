4513d31425acaab732e17282138bea696c90faf1
groomigroom <googjohn0143@gmail.com>
2026-08-12 13:38:17 +0900

    Update UserEntity.java

--- 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/entity/UserEntity.java
+++ 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/entity/UserEntity.java
@@ -1,3 +1,10 @@
 @Entity
 @EntityListeners(AuditingEntitiyListener.class)
 @Table(name = "user_user_entity")
+@Getter
+@Builder
+@NoArgsConstuctor
+@AllArgsConstuctor
+public class UserEntity {
+  @Id
+}
