3b54a349f1394c4ac02284061b57b15b4d55ef78
groomigroom <googjohn0143@gmail.com>
2026-08-13 08:50:20 +0900

    Update Product.java

--- 노트북/8월12일타임리프/프로젝트폴더/Product.java
+++ 노트북/8월12일타임리프/프로젝트폴더/Product.java
@@ -2,16 +2,23 @@ package com.example.demo.domain;
 public class Product {
     private Integer id;
     private String name;
-    private Integer price;
+    private int price;
+    private String description;
 
-    public Product(Integer id, String name, Integer price) {
-        this.id = id;
+    // 생성자
+    public Product(String name, int price, String description) {
         this.name = name;
         this.price = price;
+        this.description = description;
     }
 
-    // Getter, Setter
+    // Getter & Setter (타임리프에서 데이터를 읽기 위해 필수)
     public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
     public String getName() { return name; }
-    public Integer getPrice() { return price; }
+    public void setName(String name) { this.name = name; }
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 }
