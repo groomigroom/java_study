21dddaaff3b819297dd0b9a97ade7eb459a4b63f
groomigroom <googjohn0143@gmail.com>
2026-08-16 21:02:28 +0900

    Update UserRepository.java

--- 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/repository/UserRepository.java
+++ 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/repository/UserRepository.java
@@ -1,1 +1,8 @@
-UserRepository.java
+패키지 뭐라 뜨고;
+
+import org.example.backend.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+  
+}
