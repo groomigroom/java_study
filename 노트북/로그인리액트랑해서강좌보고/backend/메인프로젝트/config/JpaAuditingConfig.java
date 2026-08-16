ab696d1f6c654738de6a2a797c6aab91997a025a
groomigroom <googjohn0143@gmail.com>
2026-08-16 21:06:27 +0900

    Update JpaAuditingConfig.java

--- 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/config/JpaAuditingConfig.java
+++ 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/config/JpaAuditingConfig.java
@@ -1,1 +1,10 @@
-JpaAuditingConfig.java
+패키지 뭐라 뜨고;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+  
+}
