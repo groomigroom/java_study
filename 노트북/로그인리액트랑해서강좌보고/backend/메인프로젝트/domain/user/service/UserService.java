f33ab5ded9556a89e8b37c6e4d4753ac01ae15e7
groomigroom <googjohn0143@gmail.com>
2026-08-17 20:04:42 +0900

    Implement UserService class with constructor

--- 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/service/UserService.java
+++ 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/service/UserService.java
@@ -1,1 +1,22 @@
-UserService.java
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 자체 로그인 회원 가입 (존재 여부)
+
+    // 자체 로그인 회원 가입
+
+    // 자체 로그인
+
+    // 자체 로그인 회원 정보 수정
+
+    // 자체/소셜 로그인 회원 탈퇴
+
+    // 소셜 로그인 (매 로그인시 : 신규 = 가입, 기존 = 업데이트)
+
+    // 자체/소셜 유저 정보 조회
+}
