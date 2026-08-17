be4409a8a8dee3c416e931b1ea533cbd086d02f0
groomigroom <googjohn0143@gmail.com>
2026-08-17 09:36:06 +0900

    Update UserEntity.java

--- 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/entity/UserEntity.java
+++ 노트북/로그인리액트랑해서강좌보고/backend/메인프로젝트/domain/user/entity/UserEntity.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@EntityListeners(AuditingEntitiyListener.class)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user_user_entity")
 @Getter
 @Builder
